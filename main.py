1fa46ed6c7551aadb71376ce27c951cb8258d452
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-10-14 17:14:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
-import pygame, sys, random
+import pygame, sys, random, uuid
 from pygame.locals import *
 from pygame import gfxdraw
 
 # we only set global to variables that will be asigned to new value 
 # HELP
 
-# Start: TAB
-# Pause: Left Shift
+# Start/Pause: Space
 #
-# Show Radar: R
-# Hide Radar: T
+# Show/Hide Radar: R
 #
-# Show Health: H
-# Hide Health: J
+# Show/Hide Health: H
+#
+# Show/Hide Analytics: A
 #
 # Add Food: F
-# Del Food: G
+# Del Food: 
 #
 # Restart: Enter (Return)
 
-pygame.init()
-
 rnd = random.randint
 
-GUI_SIZE = (600, 600)
-gx, gy = GUI_SIZE
 FOOD = []
 FPS = 60
-RADAR = False
+
+ANALYTICS = False
 HEALTH = False
+RADAR = False
 RUN = False
+
+pygame.init()
+pygame.font.init()
+
+GUI_SIZE = (1020, 650)
+gx, gy = GUI_SIZE
 DISPLAY = pygame.display.set_mode(GUI_SIZE, 0, 32)
 DISPLAY.convert_alpha()
 
-pygame.display.set_caption('Eco-System: Evolution')
+pygame.display.set_caption('Eco-System')
 
 class Rabbit:
     def __init__(self, name=None):
@@ -43,13 +46,11 @@ class Rabbit:
         self.age = 5
         self.color = (255, 255, 255)
         self.chosen_food = None
-        # Tiem related settings
         self.counter = 0
-        self.time = 0
         # End Destinations
         self.end = (rnd(0, gx), rnd(0, gy))
         # Radar
-        self.scan_area = 15
+        self.scan_area = 25
         self.radar_color = (255, 0, 0)
         # Health
         self.death = False
@@ -58,7 +59,8 @@ class Rabbit:
         # Analytics
         self.eaten_food = 0
         self.life_timer = 0
-        
+
+    ##### Coordination Config ####
 
     def distance(self, v1, v2):
         x_dist = abs(v2[0] - v1[0])
@@ -71,16 +73,8 @@ class Rabbit:
 
     def xy(self):
         return (self.x, self.y)
-
-    def search_for_food(self):
-        if FOOD:
-            for piece in FOOD:
-                # if piece is inside radar area then go to it
-                if self.distance(piece, self.xy()) <= self.scan_area:
-                    # Eat only if hungry
-                    if self.hunger < 100:
-                        self.radar_color = (0, 255, 0)
-                        self.chosen_food = piece
+    
+    #### Status Config ####
 
     def draw_stats(self):
         # move x to the right
@@ -93,11 +87,10 @@ class Rabbit:
         x_hunger = int((self.hunger / 100) * 40)
         # draw main bar
         pygame.draw.line(DISPLAY, (255, 0, 0), start, (x_pos + 40, y_pos))
-        #draw health bar
+        # draw health bar
         pygame.draw.line(DISPLAY, (0, 255, 0), start, (x_pos + x_hunger, y_pos))
 
     def update_stats(self):
-        
         if HEALTH:
             self.draw_stats()
         
@@ -106,33 +99,65 @@ class Rabbit:
             
             # Calculate life in seconds
             if not self.counter % FPS:
-                self.time += 1
+                self.life_timer += 1
+                self.hunger -= 1
 
+            '''
             # Feel hungry if 5 seconds passed without food
-            if not self.counter % (FPS * 5):
-                self.hunger -= 10
+            if not self.counter % (FPS * 2):
+                self.hunger -= 1
+            '''
             
             # Die if no food found
             if self.hunger <= 0:
                 self.death = True
+    
+    def draw_analytics(self):
+        add_text(str(self.name), 10, self.x, self.y - self.age - 25)
+        add_text(str(self.hunger), 10, self.x, self.y - self.age - 10)
+        add_text(str(self.eaten_food), 10, self.x, self.y + self.age + 5)
+        add_text(str(self.life_timer), 10, self.x, self.y + self.age + 15)
+
+
+    def radar(self):
+        if RADAR:
+            pygame.gfxdraw.aacircle(DISPLAY, self.x, self.y, self.scan_area, self.radar_color)
+            if self.chosen_food:
+                pygame.draw.line(DISPLAY, self.radar_color, self.xy(), self.chosen_food)
+
+    #### Instinct Config ####
+
+    def search_for_food(self):
+        if FOOD:
+            for piece in FOOD:
+                # if piece is inside radar area then go to it
+                if self.distance(piece, self.xy()) <= self.scan_area:
+                    # Eat only if hungry
+                    if self.hunger < 90:
+                        self.radar_color = (0, 255, 0)
+                        self.chosen_food = piece
         
     def hunt(self, isfood=True):
         # Add more speed when more hunger
         # like making him nervous
         n = 0
         self.color = (255, 255, 255)
-        if self.hunger < 60:
-            n = 1
-            self.color = (255, 200, 0)
-        if self.hunger < 40:
-            n = 2
-            self.color = (255, 150, 0)
-        if self.hunger < 30:
-            n = 3
-            self.color = (255, 100, 0)
-        if self.hunger < 20:
-            n = 4
-            self.color = (255, 50, 0)
+
+        # Key: Health
+        # Val: [n, decrease in Greens in (R, G, B) of self.color]
+        speed_states = {
+            100:[0, 0],
+            80:[1, 55],
+            60:[2, 105],
+            40:[3, 155],
+            20:[4, 205]
+            }
+        
+        for health in speed_states:
+            if self.hunger < health:
+                data = speed_states[health]
+                n = data[0]
+                self.color = (255, 255-data[1], 255)
 
         fx, fy = 0, 0
         # Random move when no food detected
@@ -169,21 +194,16 @@ class Rabbit:
                 # get new position to target
                 self.random_position()
                 # more health
-                self.hunger = 100 if self.hunger + 10 > 100 else self.hunger + 10
+                self.hunger = 100 if self.hunger + 5 > 100 else self.hunger + 5
+                self.eaten_food += 1
                 # grow the Fish BOT
                 if self.age < 15:
                     self.age += 1
                     # update scan area for radar
-                    self.scan_area = self.age * 3
+                    self.scan_area = self.age * 5
             # if not then process a random move
             else:
                 self.random_position()
-    
-    def radar(self):
-        if RADAR:
-            pygame.gfxdraw.aacircle(DISPLAY, self.x, self.y, self.scan_area, self.radar_color)
-            if self.chosen_food:
-                pygame.draw.line(DISPLAY, self.radar_color, self.xy(), self.chosen_food)
 
     def live(self):
         global DISPLAY, FOOD
@@ -210,24 +230,35 @@ class Rabbit:
             
             # Draw Radar
             self.radar()
+
             # Update states of Rabbit
             self.update_stats()
         else:
             self.color = (255, 0, 0)
-        
+        # Draw Analytics
+        if ANALYTICS:
+            self.draw_analytics()
         pygame.draw.circle(DISPLAY, self.color, self.xy(), self.age, 0)
 
+def add_text(text, size, x, y):
+    myfont = pygame.font.SysFont('timesnewroman', size)
+    typed_text = myfont.render(text, False, (255, 255, 255))
+    rect_text = typed_text.get_rect()
+    rect_text.center = (x, y)
+    DISPLAY.blit(typed_text, rect_text)
 
 def main():
-    global RADAR, FOOD, HEALTH, RUN
+    global RADAR, FOOD, HEALTH, RUN, ANALYTICS
     count = 0
-    YELLOW =  ( 0,    255,    255)
-    BLACK = ( 0,    0,      0)
+    YELLOW = (0, 255, 255)
+    BLACK = (0, 0, 0)
 
 
     # Create a Group of Rabbits (n)
     Herd = [Rabbit() for _ in range(10)]
-    #for i in Herd:
+    for i in Herd:
+        # Generate Random Names
+        i.name = str(uuid.uuid4())[:8]
         #i.color = (rnd(100,255), rnd(100,255), rnd(100,255))
     
     while True:
@@ -246,14 +277,10 @@ def main():
                 # RESTART
                 if event.key == K_RETURN:
                     Herd = [Rabbit() for _ in range(10)]
+                    for i in Herd:
+                        i.name = str(uuid.uuid4())[:8]
                     RUN = False
                     FOOD = []
-
-                # Start or Pause
-                if event.key == K_TAB:
-                    RUN = True
-                if event.key == K_LSHIFT:
-                    RUN = False
 
                 # Show or Hide Food
                 if event.key == K_f:
@@ -261,18 +288,34 @@ def main():
                         FOOD.append((rnd(0, gx - 3), rnd(0, gy - 3)))
                 if event.key == K_g:
                     FOOD = []
-
+                
+                # Start or Pause
+                if event.key == K_SPACE:
+                    if RUN:
+                        RUN = False
+                    else:
+                        RUN = True
+                
                 # Show or Hide Radar
                 if event.key == K_r:
-                    RADAR = True
-                if event.key == K_t:
-                    RADAR = False
+                    if RADAR:
+                        RADAR = False
+                    else:
+                        RADAR = True
 
                 # Show or Hide Health
                 if event.key == K_h:
-                    HEALTH = True
-                if event.key == K_j:
-                    HEALTH = False
+                    if HEALTH:
+                        HEALTH = False
+                    else:
+                        HEALTH = True
+                
+                # Show or Hide Health
+                if event.key == K_a:
+                    if ANALYTICS:
+                        ANALYTICS = False
+                    else:
+                        ANALYTICS = True
         
         # Random Food
         if RUN:
